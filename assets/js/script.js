0c1fe8ae31b928794f595d9d0f13b9e5b2bb83e8
Ian Mucheru <imucheru.im@gmail.com>
2023-08-26 12:07:56 +0300

    Updated Preloader

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+"use strict";
+
+/*** Preload ***/
+/** Loading Will End Once The Page Loads **/
+const preloader = document.querySelector("[data-preload");
+
+window.addEventListener("load", function () {
+  preloader.classList.add("loade");
+  this.document.body.classList.add("loade");
+});
