1383b0509cc60a5a18342c59cc07dbf658b96dd1
Ian Mucheru <imucheru.im@gmail.com>
2023-09-02 16:30:49 +0300

    Updated About Section

--- assets/js/script.js
+++ assets/js/script.js
@@ -118,3 +118,23 @@ addEventOnElements(
 );
 
 window.addEventListener("load", autoSlide);
+
+/** Parallax Effect **/
+const parallaxItems = document.querySelectorAll("[data-parallax-item]");
+
+let x, y;
+
+window.addEventListener("mousemove", function (event) {
+  x = (event.clientX / window.innerWidth) * 10 - 5;
+  y = (event.clientY / window.innerHeight) * 10 - 5;
+
+  // Reverse The Number eg. 20 -> -20, -5 -> 5
+  x = x - x * 2;
+  y = y - y * 2;
+
+  for (let i = 0, len = parallaxItems.length; i < len; i++) {
+    x = x * Number(parallaxItems[i].dataset.parallaxSpeed);
+    y = y * Number(parallaxItems[i].dataset.parallaxSpeed);
+    parallaxItems[i].style.transform = `translate3d(${x}px, ${y}px, 0px)`;
+  }
+});
