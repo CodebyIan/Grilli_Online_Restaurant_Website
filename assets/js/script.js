c5ff35bd955b2e60997ecd36c4e0cd315071cd4e
Ian Mucheru <imucheru.im@gmail.com>
2023-09-08 20:33:32 +0300

    Updated Back To Top Button

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,9 +30,10 @@ const toggleNavbar = function () {
 
 addEventOnElements(navTogglers, "click", toggleNavbar);
 
-/** Show Or Hide Header **/
+/** Show Or Hide Header & Back To Top Btn **/
 
 const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
 let lastScrollPos = 0;
 
 const hideHeader = function () {
@@ -49,9 +50,11 @@ const hideHeader = function () {
 window.addEventListener("scroll", function () {
   if (window.scrollY >= 50) {
     header.classList.add("active");
+    backTopBtn.classList.add("active");
     hideHeader();
   } else {
     header.classList.remove("active");
+    backTopBtn.classList.remove("active");
   }
 });
 
